56927374fe8f48d1c2e2cf391c8fab3c1b693e2d
seniya <seniyathri@gmail.com>
2024-03-18 22:58:35 +0100

    feat; the first snack has been done the greater number is been displayed in the console

---
+++ snacks1/app.js
@@ -0,0 +1,26 @@
+console.log("hello-world");
+
+//L'utente inserisce un numero
+//con i promt
+//il software stampa il maggiore
+
+//creare 2 variable di promt
+//1 numero promt
+const number1 = parseInt(prompt("inserici un numbero")); //number
+
+//2 numero promt
+const number2 = parseInt(prompt("inserici un numbero")); //number
+
+// salvare i 2 varaibli dentro un promt
+console.log(number1, number2, typeof number1, typeof number2);
+
+//SE numbero 1 e > numbero 2
+//-cl il numbero 1 e maggiore
+//ALTRIMENTI
+//-il numero 2 e maggiore
+
+if (number1 > number2) {
+  console.log(`il numbero ${number1} e maggiore`);
+} else {
+  console.log(`il numbero ${number2} e maggiore `);
+}
